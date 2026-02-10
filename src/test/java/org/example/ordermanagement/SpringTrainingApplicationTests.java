0a12546ae7eabd0aa51293bc61fa8edffc3b75d0
AP\luong11.nguyen <luong11.nguyen@global.ntt>
2026-02-10 16:40:27 +0700

    first commit

---
+++ src/test/java/org/example/ordermanagement/SpringTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ordermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
