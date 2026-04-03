c345f8fe24eadf1f7fd616c70b1725a6c4ac21d5
WinnDers <kiienn149@gmail.com>
2026-04-03 20:42:35 +0700

    [order-bussiness-rule] add order bussiness rule

--- src/main/java/org/example/ordermanagement/service/implement/OrderBusinessRuleGuard.java
+++ src/main/java/org/example/ordermanagement/service/implement/OrderBusinessRuleGuard.java
@@ -1,7 +1,64 @@
 package org.example.ordermanagement.service.implement;
 
-public class OrderBusinessRuleGuard {
-    public void validateUpdate(){
+import org.example.ordermanagement.common.enums.OrderStatus;
+import org.example.ordermanagement.model.domain.Order;
 
+
+import org.example.ordermanagement.common.enums.ErrCode;
+import org.example.ordermanagement.exception.AppException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderBusinessRuleGuard {
+
+    public void validateUpdate(Order order, OrderStatus newStatus,
+                               boolean isAdmin, boolean isStaff, boolean isCustomer) {
+
+        if (isCustomer) {
+            throw new AppException(ErrCode.CUSTOMER_CANNOT_UPDATE);
+        }
+
+        if (!isAdmin && !isStaff) {
+            throw new AppException(ErrCode.ACCESS_DENIED);
+        }
+
+        OrderStatus oldStatus = order.getStatus();
+
+        if (oldStatus == OrderStatus.COMPLETED) {
+            throw new AppException(ErrCode.ORDER_COMPLETED_LOCKED);
+        }
+
+        if (isStaff && oldStatus == OrderStatus.CANCELLED) {
+            throw new AppException(ErrCode.ORDER_CANCELLED_CANNOT_UPDATE);
+        }
+
+        if (oldStatus == OrderStatus.CANCELLED) {
+            if (!isAdmin) {
+                throw new AppException(ErrCode.ORDER_CANCELLED_ADMIN_ONLY);
+            }
+            if (newStatus != OrderStatus.PROCESSING) {
+                throw new AppException(ErrCode.ORDER_CANCELLED_ONLY_PROCESSING);
+            }
+        }
+
+        ensureValidTransition(oldStatus, newStatus, isAdmin);
+    }
+    private void ensureValidTransition(OrderStatus oldStatus, OrderStatus newStatus, boolean isAdmin) {
+        boolean isValid = false;
+
+        if (oldStatus == OrderStatus.CREATED) {
+            if (newStatus == OrderStatus.PROCESSING || newStatus == OrderStatus.CANCELLED) {
+                isValid = true;
+            }
+        } else if (oldStatus == OrderStatus.PROCESSING) {
+            if (newStatus == OrderStatus.COMPLETED) {
+                isValid = true;
+            }
+        }
+
+        if (!isValid) {
+            throw new AppException(ErrCode.INVALID_STATUS_TRANSITION);
+
+        }
     }
 }
